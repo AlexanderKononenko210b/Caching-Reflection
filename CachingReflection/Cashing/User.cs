45985258811a0b6efc748e19c0d1f08b2bdcdad4
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2018-08-05 19:11:45 +0300

    completed task

---
+++ CachingReflection/Cashing/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Cashing
+{
+    /// <summary>
+    /// Represents a model of the User class.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second name.
+        /// </summary>
+        public string SecondName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the additional info.
+        /// </summary>
+        public AdditionInfo AdditionInfo { get; set; }
+
+        /// <summary>
+        /// Get user instance as string.
+        /// </summary>
+        /// <returns>string performance user instance</returns>
+        public override string ToString()
+        {
+            return
+                $"FirstName = {this.FirstName}, SecondName = {this.SecondName}, Age = {this.Age}," +
+                $" Phone = {AdditionInfo.Phone}, Fax = {AdditionInfo.Fax}";
+        }
+    }
+}
---
+++ CachingReflection/CashingReflection.Test.cs/EquatableComparer.Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using NUnit;
+using NUnit.Framework;
+using CachingReflection;
+
+namespace CashingAndReflections
+{
+    [TestFixture]
+    public class EquatableComparerTests
+    {
+        public IEnumerable<TestCaseData> EqualitySource
+        {
+            get
+            {
+                yield return new TestCaseData(3, 5, false);
+                yield return new TestCaseData(5, 5, true);
+                yield return new TestCaseData(new Box { X = 2, Y = 5, Size = 56, Location = "Saransk" }, new Box { X = 2, Y = 5, Size = 56, Location = "Saransk" }, true);
+                yield return new TestCaseData(new Box { X = 2, Y = 5, Size = 56, Location = "Saransk" }, new Box { X = 2, Y = 6, Size = 56, Location = "Saransk" }, false);
+            }
+        }
+
+        [TestCaseSource(nameof(EqualitySource))]
+        public void TestMethod1(object first, object second, bool expectedResult)
+        {
+            
+        }
+
+        private class Box
+        {
+            public int X { get; set; }
+
+            public int Y { get; set; }
+
+            public int Size { get; set; }
+
+            public string Location { get; set; }    
+        }
+
+    }
+}
